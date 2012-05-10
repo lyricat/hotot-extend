b8a88d0f5efd325abd42fdaa51173fb46e63d681
ben2k1690 <ben2k1690@googlemail.com>
2012-05-10 14:50:06 +0200

    added showTweet.html for easy way to edit the template

--- showTweet.py
+++ showTweet.py
@@ -8,12 +8,29 @@ from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
+class Tweets(db.Model):
+    text_id = db.StringProperty(required=True)
+    full_text = db.StringProperty(multiline=True,required=True)
+    date = db.DateTimeProperty(auto_now_add=True)
+
 class TweetHandler(webapp.RequestHandler):
     def get(self):
         base_url = urlparse.urlparse(self.request.url)
         if hasattr(base_url, "path"):
             if base_url.path.replace('/','') != None:
                 text_id = base_url.path.replace('/','')
+                
+                result = db.GqlQuery("SELECT * FROM Tweets WHERE text_id = :1", text_id)
+                
+                for tweet in result:
+                    if hasattr(tweet, "full_text"):
+                        values = {
+                            'tweet': tweet
+                        }
+                        self.response.out.write(template.render('showTweet.html', values))
+                    else:
+                        break
+                    
 
 def main():
     app = webapp.WSGIApplication([
