0c003e7dab6e0933ec38b5c3f4ebd8eb4d6d8471
ben2k1690 <ben2k1690@googlemail.com>
2012-05-12 11:07:34 +0200

    fixed 500 character limit

--- model.py
+++ model.py
@@ -3,5 +3,5 @@ from google.appengine.ext import db
 class Tweets(db.Model):
     name = db.StringProperty(required=True)
     avatar = db.StringProperty(required=True)
-    full_text = db.StringProperty(multiline=True,required=True)
+    full_text = db.TextProperty(required=True)
     date = db.DateTimeProperty(auto_now_add=True)
